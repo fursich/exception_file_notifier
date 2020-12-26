9bc8f3b524822eb6876c1b9ba40a827d48d34446
Koji Onishi <fursich0@gmail.com>
2020-12-26 21:15:46 +0900

    Add support for Ruby 3, drop support for Ruby <= 2.3.x

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,12 @@ require "bundler/setup"
 require "exception_file_notifier"
 require 'exception_notification'
 
+if RUBY_VERSION >= '2.7.2'
+  # NOTE: https://bugs.ruby-lang.org/issues/17000
+  # this will keep us informed of deprecation warnings after Ruby 2.7.2
+  Warning[:deprecated] = true
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
